ee79e717940c23b3cd77e5c75542d1098a64c673
Rose van der Giessen <rosevandergiessen@proton.me>
2023-03-15 15:09:58 +0100

    superclass and override child method

--- Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
+++ Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
@@ -1,27 +1,24 @@
 import product.Shirt;
 import product.Shirt.*;
 import product.Pants;
+import product.Product;
 
 public class Main {
 
     public static void main(String[] args) {
 
-      Shirt shirt = new Shirt();
-      shirt.setSize(Size.SMALL);
-
-      shirt.setBrand("Adidas");
-      shirt.setColor("Black");
-      shirt.setPrice(50);
+      Shirt shirt = new Shirt(40, "Black", "Armed Angels", Size.SMALL);
 
       shirt.fold();
 
-      Pants pants = new Pants();
-      pants.setWaist(32);
-      pants.setLength(32);
-      pants.setColor("Black");
-      pants.setPrice(100);
-      pants.setBrand("Levi's");
+      Pants pants = new Pants(130, "Black", "levi's", 32, 32);
 
       pants.fold();
+      productStore(pants);
+
+    }
+
+    public static void productStore(Product product) {
+      System.out.println("Thank you for purchasing the " + product.getBrand() + " " + product.getClass().getSimpleName().toLowerCase() + ".");
     }
   }
--- Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/product/Shirt.java
+++ Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/product/Shirt.java
@@ -1,16 +1,28 @@
 package product;
 
 public class Shirt extends Product {
+  private Size size;
+  public enum Size {SMALL, MEDIUM, LARGE}
 
-    private Size size;
-    public enum Size {SMALL, MEDIUM, LARGE}
+  public Shirt(double price, String color, String brand, Size size) {
+    super(price, color, brand);
+    this.size = size;
+  }
 
-    public Size getSize() {
-        return this.size;
-    }
+  public Size getSize() {
+      return this.size;
+  }
 
-    public void setSize(Size size) {
-        this.size = size;
-    }
+  public void setSize(Size size) {
+      this.size = size;
+  }
+
+  @Override
+  public void fold() {
+    super.fold();
+    System.out.println("Lay shirt on a flat surface");
+    System.out.println("Fold shirt sideways");
+    System.out.println("Fold from bottom up");
+  }
 
 }
