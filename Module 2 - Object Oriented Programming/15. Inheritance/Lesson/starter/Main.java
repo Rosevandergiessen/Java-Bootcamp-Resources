1886c87393dd11792d41cce5eb9729319d53ea1a
Rose van der Giessen <rosevandergiessen@proton.me>
2023-03-15 13:44:35 +0100

    Inheritance

--- Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
+++ Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
@@ -1,9 +1,27 @@
+import product.Shirt;
+import product.Shirt.*;
+import product.Pants;
+
 public class Main {
 
     public static void main(String[] args) {
 
+      Shirt shirt = new Shirt();
+      shirt.setSize(Size.SMALL);
 
+      shirt.setBrand("Adidas");
+      shirt.setColor("Black");
+      shirt.setPrice(50);
 
+      shirt.fold();
+
+      Pants pants = new Pants();
+      pants.setWaist(32);
+      pants.setLength(32);
+      pants.setColor("Black");
+      pants.setPrice(100);
+      pants.setBrand("Levi's");
+
+      pants.fold();
     }
-
-}
+  }
