d3aba6b161cd52dfe5d2468ad5b14499205c94d5
Rose van der Giessen <rosevandergiessen@proton.me>
2023-03-15 11:58:00 +0100

    hashmaps

--- Module 2 - Object Oriented Programming/11. Map Collections/Lessons/map-collections/Inventory.java
+++ Module 2 - Object Oriented Programming/11. Map Collections/Lessons/map-collections/Inventory.java
@@ -1,12 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inventory {
     public static void main(String[] args) {
 
+      Map<String, Double> inventory = new HashMap<>();
+      
         // inventory.put("Bananas", 2.99);
         // inventory.put("Papaya", 5.99);
         // inventory.put("Kale", 4.99);
         // inventory.put("Apples", 1.99);
         // inventory.put("Carrot", 1.49);
-        
+
         // inventory.put("Lime", 0.49);
         // inventory.put("Toothpaste", 5.99);
         // inventory.put("Zucchini", 5.99);
