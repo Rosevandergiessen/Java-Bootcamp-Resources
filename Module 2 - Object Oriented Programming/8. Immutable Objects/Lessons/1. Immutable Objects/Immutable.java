3a1787b023039ff8af466e81baf34880415fe6ae
Rose van der Giessen <rosevandergiessen@proton.me>
2023-03-15 12:02:08 +0100

    immutable objects complete

--- Module 2 - Object Oriented Programming/8. Immutable Objects/Lessons/1. Immutable Objects/Immutable.java
+++ Module 2 - Object Oriented Programming/8. Immutable Objects/Lessons/1. Immutable Objects/Immutable.java
@@ -1,5 +1,5 @@
 public class Immutable {
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
 
         int apples = 5;
         Integer applesWrapper = 5;
