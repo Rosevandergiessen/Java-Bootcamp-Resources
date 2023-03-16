499f4504b3020dd15b70750652c22e9d92c382a6
Rose van der Giessen <rosevandergiessen@proton.me>
2023-03-16 14:04:05 +0100

    comparator implemented

--- Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Two/Main.java
+++ Module 2 - Object Oriented Programming/16. Higher Order Functions/Lessons/Lesson Two/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> integers = Arrays.asList(25, 19, 23, 45, 38, 23, 59, 12);
-
+        // ascending order
+        integers.sort((right, left) -> right.compareTo(left));
+        // descending order
+        // integers.sort((right, left) -> left.compareTo(right));
+        System.out.println(integers);
 
     }
 
@@ -13,7 +17,7 @@ public class Main {
 
 
 /**************IMPERATIVE APPROACH**************
- * 
+ *
         for (int i = 0; i < integers.size(); i++) {
             for (int j = i + 1; j < integers.size(); j++) {
                 if (integers.get(i).compareTo(integers.get(j)) > 0) {
